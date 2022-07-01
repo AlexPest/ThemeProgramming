cb0a3ebcbdb2e826c7226522ce6bef8839c13772
AlexPest <alxndr2000@gmail.com>
2022-07-01 03:38:07 +0300

    Initial commit

---
+++ Theme1_Variables.py
@@ -0,0 +1,11 @@
+box1 = 5
+box2 = 15
+box3 = box1 + box2
+print(box3)
+
+name = input('Введите имя: ')
+age = input('Введите сколько вам лет: ')
+name = 'Вас зовут: ' + name
+age = 'Вам: ' + age + ' лет'
+print(name)
+print(age)
